41bacff23fae935e552e93413f6bbbcd96d7a70f
Gabriel Rodrigues de Rodrigues <gabriel.archangel369@gmail.com>
2021-12-24 09:27:24 -0300

    Digite alguma coisa, e ira printar na tela, o que é

---
+++ variavel.py
@@ -0,0 +1,12 @@
+variavel = input('Digite algo: ')
+
+print('O tipo de valor digitado é do tipo: ', type(variavel))
+print('Só tem espaço: ', variavel.isspace())
+print('É numerico: ', variavel.isnumeric())
+print('É alfabetico: ', variavel.isalpha())
+print('É alfanumerico: ', variavel.isalnum())
+print('É somente maiuscula: ', variavel.isupper())
+print('É somente minusculas: ', variavel.islower())
+print('É capitalizada: ', variavel.istitle())
+print('É decimal: ', variavel.isdecimal())
+
